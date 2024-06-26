94409ee6ae961ab857b49e04c3d760ca7665a4a8
Nikita <niknakels@gmail.com>
2024-06-26 13:57:27 +1100

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,17 @@
+# Задача "Однокоренные"
+
+# Функция поиска "однокоренных" слов
+def single_root_words(root_word: object, *other_words: object):
+    same_words = []
+    for i in other_words:
+        if root_word.lower() in i.lower():
+            same_words.append(i)
+        elif i.lower() in root_word.lower():
+            same_words.append(i)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+print(result1)
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result2)
